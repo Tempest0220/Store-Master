5d100c4c0f15894f84d4dd67ba3771d379c29c2c
D1149724 <ken20040715@gmail.com>
2025-05-16 16:40:38 +0800

    1

--- src/main/java/org/example/Frame/Product.java
+++ src/main/java/org/example/Frame/Product.java
@@ -1,7 +1,20 @@
 package org.example.Frame;
 
-public interface Product {
-    String Name();
-    int ShelvesAmount();
-    double Price();
+public abstract class Product {
+    private String name;
+    private double price;
+    private int ShelvesAmount; // 商品庫存數量
+
+    public Product(String name, double price, int ShelvesAmount) {
+        this.name = name;
+        this.price = price;
+        this.ShelvesAmount = ShelvesAmount;
+    }
+
+    public String getName() { return name; }
+    public double getPrice() { return price; }
+    public int getShelvesAmount() { return ShelvesAmount; }
+
+   
 }
+
