fc34ee12f2efb75ae7fb7deb30b17be8226c7e0f
D1149427邱禾朋 <paul1612112911@gmail.com>
2025-05-30 12:49:36 +0800

    add the ProductFolder, the Composite design pattern of Product

--- src/main/java/org/example/Frame/Product.java
+++ src/main/java/org/example/Frame/Product.java
@@ -7,23 +7,23 @@ public class Product{
     int getId(){
         return id;
     }
-    String getName(){
+    public String getName(){
         return name;
     }
-    int getPrice(){
+    public int getPrice(){
         return price;
     }
     private int amount;
-    void addNum(int num){
+    public void addNum(int num){
         if (num < 0) throw new IllegalArgumentException("數量不可為負");
         this.amount += num;
     }
-    void substractNum(int num){
+    public void substractNum(int num){
         if (num < 0) throw new IllegalArgumentException("數量不可為負");
         if (this.amount < num) throw new ProductNotEnoughException("庫存不足");
         this.amount -= num;
     }
-    int getAmount(){
+    public int getAmount(){
         return amount;
     }
     Product(int id, String name, int price){
@@ -39,8 +39,23 @@ public class Product{
         amount = product.amount;
         price = product.price;
     }
+
+
+    protected String repeat(int count, String with){
+        return new String(new char[count]).replace("\0", with);
+    }
     public String toString(){
-        return "Product(id: " + id + ", name: " + name + ", amount: " + amount + ", price: " + price + ")";
+        return "Product(id: " + id +
+                ", name: " + name +
+                ", amount: " + amount +
+                ", price: " + price +
+                ")";
+    }
+    public void display(int hierarchy){
+        System.out.println(repeat(hierarchy, "  ") + this);
+    }
+    public void display(){
+        display(0);
     }
 }
 
---
+++ src/main/java/org/example/Frame/ProductFolder.java
@@ -0,0 +1,56 @@
+package org.example.Frame;
+
+import java.util.ArrayList;
+
+public class ProductFolder extends Product{
+    private ArrayList<Product> components;
+    ProductFolder(int id, String name) {
+        super(id, name, 0);
+        components = new ArrayList<>();
+    }
+    public int getPrice(){
+        throw new UnsupportedOperationException("folder don't have price");
+    }
+    public void addNum(int num){
+        throw new UnsupportedOperationException("folder cannot add amount");
+    }
+    public void substractNum(int num){
+        throw new UnsupportedOperationException("folder cannot substract amount");
+    }
+    public int getAmount(){
+        throw new UnsupportedOperationException("folder don't have amount");
+    }
+    public void add(Product productToAdd){
+        components.add(productToAdd);
+    }
+
+
+    public String toString(){
+        return  "ProductFolder(id: " +
+                getId() +
+                ", name: " + getName() +
+                ")";
+    }
+    public void display(int hierarchy){
+        System.out.println(repeat(hierarchy, "  ") + this);
+        for (Product p : components){
+            p.display(hierarchy + 1);
+        }
+    }
+
+    // 使用測試
+    public static void main(String[] args){
+        ProductFolder root = new ProductFolder(10, "文具店庫存");
+        ProductFolder stationary = new ProductFolder(11, "文具類");
+        stationary.add(new Product(1000, "鉛筆", 20));
+        stationary.add(new Product(1001, "橡皮擦", 10));
+        root.add(stationary);
+        ProductFolder books = new ProductFolder(11, "書類");
+        books.add(new Product(1002, "字典", 100));
+        books.add(new Product(1003, "筆記本", 30));
+        root.add(books);
+        root.display();
+
+    }
+
+}
